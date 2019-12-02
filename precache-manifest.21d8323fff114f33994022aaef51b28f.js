c1028a1f8549c03d5c40801e4ee278f5801c1b20
omobosteven <omobosteven@gmail.com>
2019-12-02 14:23:02 +0100

    Updates

---
+++ precache-manifest.21d8323fff114f33994022aaef51b28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefcf6c9d9c3d4523471fc02a89f0fa5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ebc424957f9cd0a6140",
+    "url": "/monsters-rolodex/static/css/main.eb8f0500.chunk.css"
+  },
+  {
+    "revision": "2ea260805c2d8eeb767a",
+    "url": "/monsters-rolodex/static/js/2.bce2a7e5.chunk.js"
+  },
+  {
+    "revision": "1ebc424957f9cd0a6140",
+    "url": "/monsters-rolodex/static/js/main.5207b520.chunk.js"
+  },
+  {
+    "revision": "64f1edcea64d35540f1f",
+    "url": "/monsters-rolodex/static/js/runtime-main.cefde4a2.js"
+  }
+]);
